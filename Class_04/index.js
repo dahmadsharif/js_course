161ffc01ff5ad4487071e4f944ba7506d017b00d
theahmadsharif <theahmadsharif@gmail.com>
2020-09-12 15:37:48 +0600

    Fourth class

---
+++ Class_04/index.js
@@ -0,0 +1,29 @@
+let x = 1;
+if(true) {
+	 x = 2;
+}
+// console.log(x);
+/* Let Scope */
+
+
+
+
+let q = 22;
+function scope () {
+ 	 q = 10;
+}
+
+
+
+scope();
+console.log(q);
+
+console.clear();
+
+
+function hello () {
+	var a = 10;
+}
+
+hello();
+console.log(a);
