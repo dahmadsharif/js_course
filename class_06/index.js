e9a33ad34a47b225afd8658b2837c8b315ac86cf
theahmadsharif <theahmadsharif@gmail.com>
2020-09-18 09:12:23 +0600

    ecommerce ui

---
+++ class_06/index.js
@@ -0,0 +1,21 @@
+let object = [{
+	"company_info": {
+		"name": "KFC",
+		"license_no": "ER5982"
+	},
+	"contact_info": {
+		"phone": "+8801711223344",
+		"email": "admin@kfc.com"
+	}},
+	{
+	"company_info": {
+		"name": "Macdonals",
+		"license_no": "TR5582"
+	},
+	"contact_info": {
+		"phone": "+8801711223344",
+		"email": "admin@macdonals.com"
+	}}
+];
+
+
